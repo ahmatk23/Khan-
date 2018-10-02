2878762f4d7892a0b99f0fe44c8f55003ba7dbe7
ahmatk23 <43385208+ahmatk23@users.noreply.github.com>
2018-10-02 14:03:17 -0400

    Create dinner.js

---
+++ dinner.js
@@ -0,0 +1,29 @@
+stroke(66, 23, 23);
+background(186, 145, 20); // wooden table
+
+//plate
+stroke(222, 206, 206);
+fill(143, 143, 75);
+ellipse(200, 200, 350, 350); // plate
+
+fill(46, 35, 46);
+ellipse(200, 200, 300, 300); 
+
+//eggs
+stroke(77, 67, 67);
+fill(800, 800, 800);
+ellipse (150,150,50,100); // eggs
+ellipse (250,150,50,100); // eggs
+
+// grapes
+stroke(179, 3, 214);
+fill(82, 0, 82);
+ellipse (200, 230, 20, 20); // grape
+ellipse (230, 230, 20, 20);
+ellipse (260, 230, 20, 20);
+ellipse (150, 230, 20, 20);
+
+//carrot
+stroke(194, 88, 31);
+fill(255, 119, 0);
+ellipse (200, 280, 200, 20);
